d482d6e0c53afe825b041b4a6f5a8c2759ee2b48
Саша <semenov@skeeks.com>
2017-11-18 23:03:06 +0300

    SkeekS CMS 5.0.0-alpha4

--- common/config/db.php
+++ common/config/db.php
@@ -10,9 +10,12 @@ return
     'components'    => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=app-basic',
+            /*'dsn' => 'mysql:host=localhost;dbname=app-basic_2',
             'username' => 'app-basic',
-            'password' => 'hDZwcmEYc64vBtEB',
+            'password' => 'hDZwcmEYc64vBtEB',*/
+            'dsn' => 'pgsql:host=localhost;dbname=test',
+            'username' => 'test',
+            'password' => 'gk-wIJI4OMIYqOJ4EfW1jD1Yj',
             'charset' => 'utf8',
             'enableSchemaCache' => true,
             'schemaCacheDuration' => 3600,
